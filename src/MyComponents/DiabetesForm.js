876c158fa472b264d8f10d725a8a2b5b55f24260
Inderpreet02 <inder192002@gmail.com>
2022-04-23 11:40:00 +0530

    frontend done

--- src/MyComponents/DiabetesForm.js
+++ src/MyComponents/DiabetesForm.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TransactionContext } from '../context/TransactionContext'
 import "./CancerForm.css"
 
 const DiabetesForm = () => {
+
+	const { formData, setFormData, handleChange, sendTransaction } = useContext(TransactionContext);
+
+	const handleSubmit = (e) => {
+
+		e.preventDefault();
+
+		sendTransaction();
+	}
   return (
     <div class="container">
 	<div class="header">
@@ -10,51 +20,51 @@ const DiabetesForm = () => {
 	<form id="form" class="form">
 		<div class="form-control">
 			<label for="username">Full Name:</label>
-			<input type="text"  id="username" autocomplete="off" />
+			<input type="text" name="fullName"  id="username" handleChange={handleChange} autoComplete="off" />
 		
 		</div>
         <div class="form-control">
 			<label for="username">Pregnancies:</label>
-			<input type="text"  id="username" autocomplete="off" />
+			<input type="text" name="Pregnancies"  handleChange={handleChange} id="username" autoComplete="off" />
 		
 		</div>
         <div class="form-control">
 			<label for="username">Glucose :</label>
-			<input type="text"  id="username" autocomplete="off" />
+			<input type="text" name="Glucose"  handleChange={handleChange} id="username" autoComplete="off" />
 		
 		</div>
         <div class="form-control">
 			<label for="username">BloodPressure :</label>
-			<input type="text"  id="username" autocomplete="off" />
+			<input type="text" name="BloodPressure"  handleChange={handleChange} id="username" autoComplete="off" />
 		
 		</div>
         <div class="form-control">
 			<label for="username">SkinThickness :</label>
-			<input type="text"  id="username" autocomplete="off" />
+			<input type="text" name="SkinThickness"  handleChange={handleChange} id="username" autoComplete="off" />
 		
 		</div>
         <div class="form-control">
 			<label for="username">Insulin :</label>
-			<input type="text"  id="username" autocomplete="off" />
+			<input type="text" name="Insulin"  id="username" handleChange={handleChange} autoComplete="off" />
 		
 		</div>
         <div class="form-control">
 			<label for="username">BMI :</label>
-			<input type="text"  id="username" autocomplete="off" />
+			<input type="text" name="BMI" id="username" handleChange={handleChange} autoComplete="off" />
 		
 		</div>
         <div class="form-control">
 			<label for="username">DiabetesPedigreeFunction :</label>
-			<input type="text"  id="username" autocomplete="off" />
+			<input type="text"  id="username" name="DiabetesPedigreeFunction" handleChange={handleChange} autoComplete="off" />
 		
 		</div>
         <div class="form-control">
 			<label for="username">Age :</label>
-			<input type="text"  id="username" autocomplete="off" />
+			<input type="text"  id="username" name="Age" handleChange={handleChange} autoComplete="off" />
 		
 		</div>   
 
-<button>SUBMIT</button>
+<button onClick={handleSubmit} type="button">SUBMIT</button>
 </form>
 </div>
 )
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { TracsactionProvider } from './context/TransactionContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <TracsactionProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TracsactionProvider>
 );
 
 
